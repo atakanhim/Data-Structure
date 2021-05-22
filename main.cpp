7cc725e1bc1dc314e06230c2b090c9f450e3eaeb
atakan <atakanhim@gmail.com>
2021-05-22 23:14:42 +0300

    Dairesel
    Dairesel

--- main.cpp
+++ main.cpp
@@ -10,27 +10,61 @@ struct node
 };
 void show(node *roottke)
 {
-    while (roottke != NULL)
+    node *iter = roottke;
+    printf("%d \n", iter->x);
+    iter = iter->next;
+    while (iter != roottke)
     {
-        printf("%d \n", roottke->x);
-        roottke = roottke->next;
+        printf("%d \n", iter->x);
+        iter = iter->next;
     }
 }
 void ekle(node *roottke, int i)
 {
-    while (roottke->next != NULL)
-        roottke = roottke->next;
-    roottke->next = (node *)malloc(sizeof(node));
-    roottke->next->x = i;
-    roottke->next->next = NULL;
+    node *iter = roottke;
+    while (iter->next != roottke)
+        iter = iter->next;
+    iter->next = (node *)malloc(sizeof(node));
+    iter->next->x = i;
+    iter->next->next = roottke;
+}
+node *sil(node *roottke, int x)
+{
+    node *temp;
+    node *iter = roottke;
+
+    if (roottke->x == x)
+    {
+        while (iter->next != roottke)
+        {
+            iter = iter->next;
+        }
+        iter->next = roottke->next;
+        free(roottke);
+
+        return iter->next;
+    }
+    while (iter->next != roottke && iter->next->x != x)
+    {
+        iter = iter->next;
+    }
+    if (iter->next == roottke)
+    {
+        printf("sayi bulunamadı \n");
+        return roottke;
+    }
+    temp = iter->next;
+    iter->next = iter->next->next;
+    free(temp);
+    return roottke;
 }
 node *siraliEkle(node *roottke, int i)
 {
 
-    if (roottke == NULL)
+    if (roottke == NULL) // bos olma durumu
     {
         roottke = (node *)malloc(sizeof(node));
-        roottke->next = NULL;
+        roottke->next = roottke;
         roottke->x = i;
         return roottke;
     }
@@ -39,10 +73,15 @@ node *siraliEkle(node *roottke, int i)
         node *temp = (node *)malloc(sizeof(node));
         temp->x = i;
         temp->next = roottke;
+        node *iter = roottke;
+        while (iter != roottke)
+            iter = iter->next;
+        iter->next = temp;
         return temp;
     }
+    //3. durum
     node *iter = roottke;
-    while (iter->next != NULL && iter->next->x < i)
+    while (iter->next != roottke && iter->next->x < i)
     {
         iter = iter->next;
     }
@@ -58,10 +97,15 @@ int main()
     int i;
     node *root;
     root = NULL;
-    root = siraliEkle(root, 300);
+
     root = siraliEkle(root, 200);
     root = siraliEkle(root, 100);
     root = siraliEkle(root, 400);
     root = siraliEkle(root, 350);
+    root = siraliEkle(root, 300);
     show(root);
+    root = sil(root,350);
+    show(root);
+
+    root = sil(root,333);
 }
